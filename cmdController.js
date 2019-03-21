b6bca99a09d593ba9a861d576bb4744b8ba10971
Jack Kelly <jack.kelly@atos.net>
2019-03-21 12:02:37 +0000

    conversation commands

--- cmdController.js
+++ cmdController.js
@@ -1,11 +1,15 @@
 "use strict";
 
-//commands to be looped t hrough
+//commands to be looped through
 const commands = [
     require('./commands/helloWorld'),
     require('./commands/demo'),
-    require('./commands/time')
+    require('./commands/time'),
+    require('./commands/conversation')
 ];
+
+//multi stage commands create one of these to store state while waiting for input
+const conversations = [];
 
 module.exports = function(client, item){
     
@@ -15,27 +19,76 @@ module.exports = function(client, item){
     };
     
     const sendMessage = function(item){
-        return client.addTextItem(item.convId, item);
+        console.log(item.content);
+        return client.addTextItem(item.convId, item.content);
     };
     
     //logic here
     if (!sentByMe(item)) {
-        let output;
-        for(let i = 0; i < commands.length; i++){
-            if(commands[i].trigger === item.text.content){
-                output = commands[i].payload()
+
+        //check if there's a currently active conversation instance for this input first
+        for(let i = 0; i < conversations.length; i++){
+            if(conversations[i].trigger === item.text.content){
+                conversations[i].payload(item)
                 .then(output => {
-                    let comment = {
-                        convId: item.convId,
-                        parentId: item.parentId,
-                        content: output
+                    //gotta love javascript, apparantly this is how you check for strings
+                    if(output.constructor === String){
+                        let comment = {
+                            convId: item.convId,
+                            content: {
+                                content: output,
+                                parentId: item.parentItemId
+                            }
+                        };
+                        sendMessage(comment);
                     };
-                    sendMessage(comment);
-                    return;
                 })
+                .catch(error => {
+                    console.log(error);
+                });
+                return;
             
             }
-        }
+        };
+
+        //if not, check commands normally
+        for(let i = 0; i < commands.length; i++){
+            if(commands[i].trigger === item.text.content){
+                commands[i].payload(item)
+                .then(async (output) => {
+                    //command started a conversation
+                    if (output instanceof Object){
+                        conversations.push(output)
+                        await output.payload(item)
+                        .then(result => {
+                            output = result;
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+                    };
+                    //gotta love javascript, apparantly this is how you check for strings
+                    if(output.constructor === String){
+                        let comment = {
+                            convId: item.convId,
+                            content: {
+                                content: output,
+                                parentId: item.parentItemId
+                            }
+                        };
+                        sendMessage(comment)
+                        .catch(err => {
+                            console.log(err);
+                        });
+                    };
+                return;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            
+            }
+        };
     };
     
 
---
+++ commands/conversation.js
@@ -0,0 +1,38 @@
+
+module.exports = {
+
+    name: "Conversation",
+
+    trigger: "!conv",
+
+    payload: async function(item){
+        return new convTest;
+    }
+
+}
+
+class convTest{
+
+    constructor(){
+        this.trigger = "!conv";
+        this.state = 0;
+        this.payload = async function(){           
+            let output = this.statechecker();
+            this.state++;
+            return output;
+        };
+
+        this.statechecker = function(){
+            switch(this.state){
+                case 0:
+                    return("State 1!");
+                case 1:
+                    return("State 2!");
+                default:
+                    return("and so on...");
+            };
+        }
+
+    }
+
+}
